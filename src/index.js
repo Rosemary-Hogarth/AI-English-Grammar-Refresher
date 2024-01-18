02d922753a39c83718e86e720564fc9951197515
Rosemary-Hogarth <144120206+Rosemary-Hogarth@users.noreply.github.com>
2024-01-18 08:56:09 +0100

    Added form handling and js typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateGrammarInfo(event) {
+  event.preventDefault();
 
+  new Typewriter("#grammar_info", {
+    strings: "I went shopping yesterday.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#grammar_term");
+formElement.addEventListener("submit", generateGrammarInfo);
